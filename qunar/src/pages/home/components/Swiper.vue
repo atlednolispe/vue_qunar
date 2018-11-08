c95f257b9a75a3882c1d1ef72b20a24ec53a2ce3
atlednolispe <atlednolispe@gmail.com>
2018-11-08 17:41:19 +0800

    图标区布局.

--- qunar/src/pages/home/components/Swiper.vue
+++ qunar/src/pages/home/components/Swiper.vue
@@ -18,7 +18,7 @@ export default {
         pagination: '.swiper-pagination',
         loop: true,
         autoplay: true,
-        speed: 2000,
+        speed: 2000
       },
       swiperList: [
         {
