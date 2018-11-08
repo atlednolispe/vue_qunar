f8d0b85e4d8b6ad786338415b4e48605a0bfa1fb
atlednolispe <atlednolispe@gmail.com>
2018-11-08 21:31:24 +0800

    周末游组件.

---
+++ qunar/src/pages/home/components/Weekend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+          title: '承载杭城的历史之地',
+          desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+          title: '承载杭城的历史之地',
+          desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+          title: '承载杭城的历史之地',
+          desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+          title: '承载杭城的历史之地',
+          desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    margin-top: .1rem
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
