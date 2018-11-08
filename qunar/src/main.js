7cd01e0071052faed4307936ec03ae079baa2a01
atlednolispe <atlednolispe@gmail.com>
2018-11-08 11:48:30 +0800

    基础项目结构.

--- qunar/src/main.js
+++ qunar/src/main.js
@@ -1,15 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 入口文件
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App' // webpack默认查找.vue .js .json
+import router from './router' // ./router/index.js
+import fastClick from 'fastclick' // npm install fastclick --save 开发,生产都使用
+import './assets/styles/reset.css' // 保证不同浏览器默认样式相同
+import './assets/styles/border.css' // 1px边框, css 1px -> x px
+// 移动端300ms点击延迟
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#app', // 挂载的是根目录下index.html的#app
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>' // = <App></App>
 })
---
+++ qunar/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<!--template下只能有一个根元素-->
+<template>
+  <div>
+    <div class="home">Home</div>
+    <router-link to="/list">List</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home {
+    font-size: 50px;
+  }
+</style>
