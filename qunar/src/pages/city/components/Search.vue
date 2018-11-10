5ba45ed7d6f08085c01bf14450904b4354e2d1ca
atlednolispe <atlednolispe@gmail.com>
2018-11-10 17:01:29 +0800

    城市搜索功能.

--- qunar/src/pages/city/components/Search.vue
+++ qunar/src/pages/city/components/Search.vue
@@ -1,7 +1,19 @@
 <!--template下只能有一个根元素-->
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或者拼音">
+      <div class="search-content" v-show="keyword">
+        <ul>
+          <li v-for="item of list" :key="item.id" class="search-item border-bottom">
+            {{ item.name }}
+          </li>
+          <li class="search-item border-bottom" v-show="hasNoData">
+            没有匹配数据
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,8 +21,43 @@
 export default {
   name: 'CitySearch',
   props: {
-    city: {
-      type: String
+    cities: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      console.log('keyword', this.keyword)
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
     }
   }
 }
@@ -31,4 +78,18 @@ export default {
       text-align: center
       border-radius: .06rem
       color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
