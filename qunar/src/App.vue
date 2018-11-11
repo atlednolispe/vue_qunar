0e28de075d0f11cedf09f1a68b2b052689ea0a17
atlednolispe <atlednolispe@gmail.com>
2018-11-11 10:39:06 +0800

    Ajax获取详情页数据并通过keep-alive配置exclude取消缓存.

--- qunar/src/App.vue
+++ qunar/src/App.vue
@@ -2,7 +2,7 @@
 <template>
   <div id="app">
     <!--显示当前路由地址对应的内容-->
-    <keep-alive>
+    <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
   </div>
--- qunar/src/pages/detail/Detail.vue
+++ qunar/src/pages/detail/Detail.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName"
+                   :bannerImg="bannerImg"
+                   :bannerImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <detail-list :list="list"></detail-list>
     <div class="content"></div>
@@ -8,6 +10,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
@@ -20,31 +23,33 @@ export default {
   },
   data () {
     return {
-      list: [
-        {
-          title: '成人票',
-          children: [
-            {
-              title: '成人票1',
-              children: [
-                {
-                  title: '成人票1-hz'
-                },
-                {
-                  title: '成人票1-sh'
-                }
-              ]
-            },
-            {
-              title: '成人票2'
-            }
-          ]
-        },
-        {
-          title: '学生票'
-        }
-      ]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
     }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
